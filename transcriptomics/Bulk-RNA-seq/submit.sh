0b6a63cdfc91e82405b1a36d058e206257f8affe
sriva <sriva@cbrglogin1>
2023-10-06 12:10:13 +0100

    submit.sh up-to-date

--- transcriptomics/Bulk-RNA-seq/submit.sh
+++ transcriptomics/Bulk-RNA-seq/submit.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#SBATCH --partition=long
-#SBATCH --job-name=bulkRNA
-#SBATCH --ntasks=4
+#SBATCH --partition=<partition-name>
+#SBATCH --job-name=<job-name>
+#SBATCH --ntasks=2
 #SBATCH --mem=64G
-#SBATCH --mail-user=simone.riva@imm.ox.ac.uk
-#SBATCH --time=06-23:59:59
+#SBATCH --mail-user=<your-email>
+#SBATCH --time=03-12:00:00
 #SBATCH --output=%j_%x.out
 #SBATCH --error=%j_%x.err
 
-source /project/Wellcome_Discovery/sriva/mambaforge/bin/activate upstream_backup
+source /path/to/baseenv/bin/activate upstream
 
 # select and edit the config file accordingly
-# Done
-snakemake --configfile=config/analysis.yaml all --cores 8 --unlock
-snakemake --configfile=config/analysis.yaml all --cores 8 --rerun-incomplete
+snakemake --configfile=config/analysis.yaml all --cores 2 --unlock
+snakemake --configfile=config/analysis.yaml all --cores 2 --rerun-incomplete
